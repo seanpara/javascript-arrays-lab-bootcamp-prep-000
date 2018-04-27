559a2a833cd4effb1856485ef7aaf3d43fabb9e8
Sean Logan Para <spara77@gmail.com>
2018-04-27 21:40:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ function destructivelyRemoveLastKitten() {kittens.pop(); return kittens}
 function destructivelyRemoveFirstKitten() {kittens.shift(); return kittens} 
 function appendKitten(name) {return [...kittens, name]}
 function prependKitten(name) {return [name, ...kittens]}
-function removeLastKitten() {return kittens.slice(0, kittens.length)
+function removeLastKitten() {return kittens.slice(0, kittens.length - 1)}
